13599207d7cbf328a5bb9ae43c01ab7c94de4cc1
ricky <136039356+riki-pedia@users.noreply.github.com>
2026-08-19 20:30:01 -0700

    resources add

--- members/nav.js
+++
@@ -1,7 +0,0 @@
-// injects navbar dynamically into any file
-fetch('../nav.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('nav').innerHTML = data
-    document.getElementById('logo').href = "../"
-})
